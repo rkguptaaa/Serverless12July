ddff85e722c62fea75a77cd6f2a62ba0a0a9ff0b
R. K. Gupta <rkguptaaa@gmail.com>
2023-07-12 08:46:18 +0530

    Update HelloWorld.vue
    changed the API URL

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@ const MAX_IMAGE_SIZE = 1000000
 /* ENTER YOUR ENDPOINT HERE
    FILES UPLOADED TO MY ENDPOINT ARE AUTOMATICALLY DELETED EVERY FEW HOURS */
 
-const API_ENDPOINT = 'https://vhnxv967a3.execute-api.us-east-1.amazonaws.com/default/myFun'
+const API_ENDPOINT = 'https://44ec10m4v1.execute-api.us-east-1.amazonaws.com/default/serverlessProjectFunction'
 
 export default {
   name: 'app',
